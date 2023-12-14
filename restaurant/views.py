5f3aeeee81de712814d4c9039a1a0224f9e375f6
Jikku Alex <iamjikkualex@gmail.com>
2023-12-14 19:34:31 +0530

    Set Up Table Booking API

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from django.shortcuts import render
 from .models import Booking, MenuItem
@@ -37,3 +38,7 @@ class MenuItemView(ListCreateAPIView):
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
