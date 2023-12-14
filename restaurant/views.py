049b5fc6ae1a531b8cc83ada6ed1097ccd2d046b
Jikku Alex <iamjikkualex@gmail.com>
2023-12-14 13:23:42 +0530

    Set Up Menu API

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,39 @@
+from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.response import Response
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Booking, MenuItem
+from .serializers import BookingSerializer, MenuItemSerializer
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class BookingView(APIView):
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data)
+
+class MenuView(APIView):
+    def get(self, request):
+        items = MenuItem.objects.all()
+        serializer = MenuItemSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = MenuItemSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status': 'success', 'data': serializer.data})
+
+class MenuItemView(ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
